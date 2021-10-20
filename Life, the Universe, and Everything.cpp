3981e3815d8ecfb78687982b6e19c048e7cdcbee
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2021-10-20 21:50:25 +0600

    Add files via upload

---
+++ Life, the Universe, and Everything.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    while(scanf("%d",&n)==1)
+    {
+        if(n==42)break;
+        else
+        {
+            cout << n << endl;
+        }
+    }
+    return 0;
+}

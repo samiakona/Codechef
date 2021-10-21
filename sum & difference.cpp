30db57542623c4b93933ea7031296512fb0ab3d1
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2021-10-21 23:24:57 +0600

    Add files via upload

---
+++ sum & difference.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N1, N2;
+    cin >> N1 >> N2;
+    if(N1>N2)
+    {
+        cout << N1-N2 <<endl;
+    }
+    else
+    {
+        cout << N1+N2 <<endl;
+    }
+
+    return 0;
+}

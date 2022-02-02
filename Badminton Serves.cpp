02ab92d974cc629558e55112321bbccee087b077
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-02-02 22:04:42 +0600

    Add files via upload

---
+++ Badminton Serves.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T,p,k,s;
+    cin>> T;
+    while(T--)
+    {
+        cin >>p;
+        k=p/2;
+        cout << k<<endl;
+        s=k+1;
+        cout << s<<endl;
+    }
+    return 0;
+}

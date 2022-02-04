b04ad9ab5104c812eeaaf1b7cdb177796948b086
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-02-04 19:36:36 +0600

    Add files via upload

---
+++ Chef and the Hair Salon.cpp
@@ -0,0 +1,18 @@
+/**
+    Author: Samia Akter Kona
+    Bangladesh University of Business and Technology (BUBT)
+**/
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T, N, M;
+    cin >> T;
+    while(T--)
+    {
+        cin >> N >> M;
+        cout << N*M << endl;
+    }
+    return 0;
+}

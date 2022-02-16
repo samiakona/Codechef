9218611c28f4560b730e0f7b6a33df39a1390531
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-02-16 22:24:43 +0600

    Add files via upload

---
+++ Count the Notebooks.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int T;
+    cin >> T;
+    while(T--)
+    {
+        int n;
+        cin >> n;
+        cout << n*10 << endl;
+    }
+    return 0;
+}

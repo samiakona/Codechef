66adba44d6bf082bf9bdd334088db33ae2df4e28
Samia_Kona <69978344+samiakona@users.noreply.github.com>
2022-02-21 22:19:29 +0600

    Add files via upload

---
+++ Guess the bottom face.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int N,T,K;
+    cin >> T;
+    while(T--)
+    {
+        cin >> N;
+        K = 7-N;
+        cout << K << endl;
+    }
+    return 0;
+}
